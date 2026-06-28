fdd2d42512c2db837e977aa7e87a9444e8e1b3b2
VasconcelosArth <arthurleit.dev@gmail.com>
2026-06-28 19:28:08 -0300

    Working with dynamic attributes

--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,17 @@ use leptos::prelude::*;
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
+    let double_count = move || count.get() * 2;
 
     view! {
         <button
-            on:click=move |_| set_count.set(count.get() + 3)
+            on:click=move |_| set_count.update(|cont| *cont += 10)
+            class:_red=move || count.get() % 2 == 1
+
+            style="position: absolute"
+            style:left=move || format!("{}px", count.get() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
+            style:max-width="400px"
         >
             "Click me: "
             {count}
@@ -14,7 +21,13 @@ pub fn App() -> impl IntoView {
 
         <p>
             "Double count: "
-            {move || count.get() * 2}
+            {double_count}
         </p>
+
+        <progress
+            max="50"
+
+            value=double_count
+        />
     }
 }
